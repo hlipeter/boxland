c9e7d68fad3f9484c446fa3fb08ea3bd39cb31d3
shihaodong <shihaodong@vjweatlh.com>
2019-12-20 17:50:27 +0800

    build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,10 @@ module.exports = {
       script: "npm run start",
       cwd: "./",
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-      args: "one two",
-      instances: 2,
+      // args: "one two",
+      instances: 1,
       autorestart: true,
-      watch: [".next"],
+      watch: [".next"], // 监听文件目录变化
       ignore_watch: ["node_modules", "logs", "static"],
       node_args: "--harmony",
       max_memory_restart: "1G",
@@ -27,7 +27,7 @@ module.exports = {
       host: "106.14.194.34",
       ref: "origin/master",
       repo: "git@github.com:hlipeter/boxland.git",
-      path: "/www/boxland/production",
+      path: "/www/boxland/",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production"
     }
