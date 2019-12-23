1ea9df0684619b1f6a44f7c5f4bcfa506bb2aa6a
shihaodong <shihaodong@vjweatlh.com>
2019-12-23 11:10:47 +0800

    ecosystem must be exist

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "boxland",
+      script: "npm run start",
+      cwd: "./",
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      args: "one two",
+      instances: 2,
+      autorestart: true,
+      watch: [".next"],
+      ignore_watch: ["node_modules", "logs", "static"],
+      node_args: "--harmony",
+      max_memory_restart: "200M",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
